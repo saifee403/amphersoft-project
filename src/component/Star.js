d43bb9d5e3ba799dab4aaa70f8c010cacaabd6bd
saifee403 <muh.saif2001@gmail.com>
2023-08-20 19:47:23 +0500

    star rating

---
+++ src/component/Star.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import {AiOutlineStar} from 'react-icons/ai'
+import {FaStar, FaStarHalfAlt} from 'react-icons/fa'
+
+const Star = ({stars, reviews}) => {
+  return (
+    <div>Star</div>
+  )
+}
+
+export default Star
