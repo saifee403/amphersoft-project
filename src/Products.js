764abf7234fefa5127012d781f44b64071d91060
saifee403 <muh.saif2001@gmail.com>
2023-08-21 11:29:29 +0500

    filter context

--- src/Products.js
+++ src/Products.js
@@ -1,13 +1,25 @@
 import React from "react";
 import styled from "styled-components";
-
+import FilterSection from "./component/FilterSection";
+import Sort from './component/Sort'
+import ProductList from './component/ProductList'
+import { useFilterContext } from "./context/filter_context";
 const Products = () => {
+  const {filter_products} = useFilterContext();
   return (
   <Wrapper>
     <div className="container grid grid-filter-column">
       <div>
-        
+        <FilterSection/>
       </div>
+      <section className="product-view--sort">
+        <div className="sort-filter">
+          <Sort/>
+        </div>
+        <div className="main-section">
+          <ProductList/>
+        </div>
+      </section>
     </div>
     </Wrapper>
     )
