a756d5f80fa8cef797966f883af2b4ed5fbfd3c5
saifee403 <muh.saif2001@gmail.com>
2023-08-21 12:57:26 +0500

    add gird view list view functionility show total product on top

--- src/reducer/filterReducer.js
+++ src/reducer/filterReducer.js
@@ -6,11 +6,18 @@ const filterReducer = (state, action) => {
                 filter_products:[...action.payload],
                 all_products: [...action.payload],
             };
-        case 'SET_GRIDVIEW':
+        case 'SET_GRID_VIEW':
             return{
                 ...state,
                 grid_view: true,
             };
+        
+        case 'SET_LIST_VIEW':
+                return{
+                    ...state,
+                    grid_view: false,
+                };
+        
 
             default:
                 return state;
